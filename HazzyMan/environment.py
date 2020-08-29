cfce56d274f768d70caf168bd1ac0778275cc37d
daniellehm <48086048+daniellehm@users.noreply.github.com>
2020-08-29 20:22:53 +1000

    playgroud change

--- HazzyMan/environment.py
+++ HazzyMan/environment.py
@@ -46,3 +46,19 @@ class Playground:
         else:
             screen.blit(self.iconCont, (self.x,self.y))
 
+
+class School:
+    
+    def __init__(self):
+        self.x = 60
+        self.y = 80
+       # self.iconCont = pygame.image.load("playground_c.png")
+        #self.iconNonCont = pygame.image.load("playground_nc.png")
+        self.infected = True
+    
+    def drawSchool(self, screen):
+        if self.infected == False:
+            screen.blit(self.iconNonCont, (self.x, self.y))
+        else:
+            screen.blit(self.iconCont, (self.x,self.y))
+
